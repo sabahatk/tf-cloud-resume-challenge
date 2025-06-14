08f6b20cd5affe40ba44b8d9f8f5fab654e74541
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2025-06-14 17:17:39 -0400

    Updated code to test both update and retrieve functions

--- tests/test_visitor_counter.py
+++ tests/test_visitor_counter.py
@@ -4,31 +4,38 @@ from src.lambda_visitor_counter import lambda_handler as update_handler
 from src.lambda_visitor_counter_retrieve_item import lambda_handler as retrieve_handler
 from moto import mock_aws # install using pip install 'moto[ec2,s3,all]'
 
+#Create a mock dynamodb table
 @mock_aws
 def test_lambda_visitor_counter():
+    #retrieve the dynamodb resource
     dynamodb = boto3.resource('dynamodb')
+    #Create a mock dynamodb table
     table = dynamodb.create_table(
         TableName="VisitCounterDB",
         KeySchema=[{'AttributeName': 'counter-id', 'KeyType': 'HASH'}],
         AttributeDefinitions=[{'AttributeName': 'counter-id', 'AttributeType': 'S'}],
         ProvisionedThroughput={'ReadCapacityUnits': 1, 'WriteCapacityUnits': 1}
     )
+    table.put_item(Item={"counter-id": "1", "counterVal": 0}) #Create an item for mock table
+    event = {"counter-id": "1", "counterVal": 0} #Event is the item
 
-    table.put_item(Item={"counter-id": "1", "counterVal": 0})
-    event = {"counter-id": "1", "counterVal": 0}
-    response = retrieve_handler(event, context={})
+    
+    response = retrieve_handler(event, context={}) #retrieve the value before updating the counter
+    body = response["body"] #retrieve only the body portion of the response
+    expected_body = "{\"body\": 0.0}" #Expected result before update
+    assert body == expected_body #Check to see if the value is correctly getting retrieveed before the counter update
 
-    body = response["body"]
-    expected_body = "{\"body\": 0.0}"
-    if body == expected_body:
+    update_handler(event, context={}) #Update the mock table counter
+    response = retrieve_handler(event, context={}) #retrieve the counter again after the update
+
+    body = response["body"] #Retrieve only the body from the response
+    expected_body = "{\"body\": 1.0}" #expected result after update
+    if body == expected_body: #Success message
         print("SUCCESS")
 
-    print("RESPONSE is:", body, "type:", type(body))
-    print("TEST Response is: ", expected_body, "type", type(expected_body))
-    print("Check values", body == expected_body)
-    assert body == expected_body
+    assert body == expected_body #Assert that the correct value happens after the counter update
 
-#test_lambda_visitor_counter()
+#test_lambda_visitor_counter() #Run the function
 
 
 
